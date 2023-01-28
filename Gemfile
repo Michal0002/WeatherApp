5a689dda0edc6ce0cc8cfd1a51d3d4a7c37c7313
Michał <ul0253566@edu.uni.lodz.pl>
2023-01-28 22:03:47 +0100

    Handle with errors when location is empty or invalid

--- Gemfile
+++ Gemfile
@@ -12,9 +12,8 @@ gem "stimulus-rails"
 gem "jbuilder"
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 gem "bootsnap", require: false
-gem 'openweather2'
 gem 'open-weather-ruby-client'
-
+gem 'geocoder', '~> 1.8', '>= 1.8.1'
 group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
 end
--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,18 +1,18 @@
 class WeatherController < ApplicationController
-    require 'openweather2'
-
     def weather
         @location =  params[:location]
-        client = OpenWeather::Client.new(
-            api_key: "787c0a5f0ae4031bcdaec058cd2e5e2f"
-          )
-          weather = client.current_city(@location)
-          @temperature = weather.main.temp_max_c
-
-          @wind = weather.wind.speed
-          @icon = weather.icon_url
-
+        if @location.empty?
+            flash[:error] = "Please enter a location."
+        else
+            begin
+                coordinates = Geocoder.coordinates(@location)
+                client = OpenWeather::Client.new(api_key:"787c0a5f0ae4031bcdaec058cd2e5e2f")
+                weather = client.current_city(@location)
+                @temperature = weather.main.temp_max_c
+                @wind = weather.wind.speed
+            rescue Faraday::ResourceNotFound => e
+                flash[:error] = "Location not found. Please enter a valid location"
+              end
+            end
+        end
     end
-
-    
-end
