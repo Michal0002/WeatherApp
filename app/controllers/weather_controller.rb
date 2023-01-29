140af16a52cea4abc8f97c4d5be4630e9e5d839e
Michał <ul0253566@edu.uni.lodz.pl>
2023-01-29 11:57:09 +0100

    Autocomplete gem

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -4,17 +4,19 @@ class WeatherController < ApplicationController
     end
     
     def weather
-        @location = params[:location] || "Warsaw" #if location is empty then return Warsaw 
+        @location = params[:location] || "Warsaw" #location is empty then return Warsaw 
         if @location.empty?
             flash[:error] = "Please enter a location."
             flash.discard
-        else
+         else
             begin
-                coordinates = Geocoder.coordinates(@location)
+                cities = fetch_cities_by_name(@location)
+
+                # coordinates = Geocoder.coordinates(@location)
                 client = OpenWeather::Client.new(api_key:"787c0a5f0ae4031bcdaec058cd2e5e2f")
                 weather = client.current_city(@location)
 
-                @temperature = weather.main.temp_max_c
+                @temperature = weather.main.temp_max_c.round(1) 
                 @wind = weather.wind.speed
                 @presure = weather.main.pressure 
 
@@ -22,6 +24,12 @@ class WeatherController < ApplicationController
             rescue Faraday::ResourceNotFound => e
                 flash[:error] = "Location not found. Please enter a valid location"
               end
-            end
-        end
+         end
     end
+    def fetch_cities_by_name(name)
+        # fetch the cities based on the name entered
+        # return the cities as an array
+        cities = ["New York", "Los Angeles", "Chicago", "Houston"]
+        cities.select { |city| city.downcase.start_with?(name.downcase) }
+      end
+end
